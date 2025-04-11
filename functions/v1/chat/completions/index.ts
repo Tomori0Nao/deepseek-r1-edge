d7f1ae50036610487682108046738eb26fca5c33
Tomori0Nao <zjhzjhanpanman@2020.cqut.edu.cn>
2025-04-11 22:53:09 +0800

    Update index.ts

--- functions/v1/chat/completions/index.ts
+++ functions/v1/chat/completions/index.ts
@@ -153,7 +153,7 @@ export async function onRequest({ request, params, env }: any) {
   try {
     // @ts-ignore-next-line
     const res = await AI.chatCompletions({
-      model: '@tx/deepseek-ai/deepseek-r1-distill-qwen-32b',
+      model: '@tx/deepseek-ai/deepseek-r1-distill-qwen-70b',
       messages: messages,
       stream: true,
     });
